1d96d4c87c47c2d87bb4da291ad938cf7b730f81
Bang Jun-young <bang.junyoung@gmail.com>
2019-08-26 17:39:07 +0900

    Revive Array2D.ofArray with a proper implementation
    The last implemention of Array2D.ofArray had another bug. Hopefully, this one should fix it.

--- src/SpiralMatrixSharp/FSharp.fs
+++ src/SpiralMatrixSharp/FSharp.fs
@@ -31,6 +31,16 @@ module Array2D =
         |> Seq.cast<'T>
         |> Seq.toArray
 
+    let ofArray nrows ncols source =
+        if Array.length source <> nrows * ncols then
+            invalidArg "source" "must have a length of nrows multiplied by ncols"
+
+        let array2D = Array2D.zeroCreate nrows ncols
+        source
+        |> Array.iteri (fun i elem -> array2D.[i / ncols, i % ncols] <- elem)
+
+        array2D
+
 module List =
     let pairwiseCyclic source =
         match source with
