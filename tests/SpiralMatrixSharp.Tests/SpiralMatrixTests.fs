3977d0a0d59c33d187346bd67da663b74fce08d4
Bang Jun-young <bang.junyoung@gmail.com>
2026-01-25 17:00:09 +0900

    Adopt new file naming convention

--- tests/SpiralMatrixSharp.Tests/SpiralMatrixTests.fs
+++ tests/SpiralMatrixSharp.Tests/SpiralMatrixTests.fs
@@ -23,7 +23,7 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-module SpiralMatrixSharp.SpiralMatrixTest
+module SpiralMatrixSharp.SpiralMatrixTests
 
 open NUnit.Framework
 
